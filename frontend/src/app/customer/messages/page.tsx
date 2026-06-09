e15aa61a167a309f3bf0ace188fe614daac222e8
Senadhi Nipun <senadhirajarathna@gmail.com>
2026-06-09 14:37:42 +0530

    modified for deploy

--- frontend/src/app/customer/messages/page.tsx
+++ frontend/src/app/customer/messages/page.tsx
@@ -1,12 +1,12 @@
-"use client";
-
-import React from "react";
+import { Suspense } from "react";
 import ChatInterface from "@/components/chat/ChatInterface";
 
 export default function CustomerMessagesPage() {
   return (
     <div className="h-[calc(100vh-5.5rem)] lg:h-[calc(100vh-6.5rem)]">
-      <ChatInterface />
+      <Suspense>
+        <ChatInterface />
+      </Suspense>
     </div>
   );
 }
